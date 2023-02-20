348d6be0346121536c94d0d35ba7191e6dd76168
dvshannon <dvshannon96@gmail.com>
2023-02-20 18:52:11 -0500

    form js, axios response in console

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
 
 const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
 const keyValueTemplate = document.querySelector('[data-key-value-template]')
+const form = document.querySelector('[data-form]')
 
 document.querySelector('[data-add-query-param-btn]').addEventListener('click', () => {
     queryParamsContainer.append(createKeyValuePair())
@@ -16,10 +18,35 @@ document.querySelector('[data-add-request-header-btn]').addEventListener('click'
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    axios({
+        url: document.querySelector('[data-url]').value,
+        method: document.querySelector('[data-method]').value,
+        params: keyValuePairsToObjects(queryParamsContainer),
+        headers: keyValuePairsToObjects(requestHeadersContainer)
+    }).then(response => {
+        // test response https://jsonplaceholder.typicode.com/todos/1
+        console.log(response)
+    })
+})
+
 function createKeyValuePair () {
-    const ele = keyValueTemplate.content.cloneNode(true)
-    ele.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
+    const element = keyValueTemplate.content.cloneNode(true)
+    element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
         e.target.closest('[data-key-value-pair]').remove()
     })
-    return ele
+    return element
+}
+
+function keyValuePairsToObjects(container) {
+    const pairs = container.querySelectorAll('[data-key-value-pair]')
+    return [...pairs].reduce((data, pair) => {
+        const key = pair.querySelector('[data-key').value
+        const value = pair.querySelector('[data-value').value
+
+        if (key === '' ) return data 
+        return { ...data, [key]: value }
+    }, {})
 }
